e4199f0d6a182c6bb3085a4e9e2118933e6f95bb
ashankar <ashankar@ashankarsp3.bayviewglen.ca>
2015-10-23 13:24:41 -0400

    blah

--- FirstGitPush/src/com/bayviewglen/gitpushes/ExampleGitCode.java
+++ FirstGitPush/src/com/bayviewglen/gitpushes/ExampleGitCode.java
@@ -3,7 +3,7 @@ package com.bayviewglen.gitpushes;
 public class ExampleGitCode {
 
 	public static void main(String[] args) {
-			System.out.println("Hi git!");
+			System.out.println("Hi Git friends!");
 	}
 
 }
